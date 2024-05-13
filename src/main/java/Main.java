69d90de8426b27d25e96009d97f80febf4470101
Beatriz Huberto <169067859+trizrto@users.noreply.github.com>
2024-05-13 16:44:05 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("********");
+    System.out.println("* You *");
+    System.out.println("********");
+
+    System.out.println("");
+    System.out.println("A história acompanha Joe, um gerente de livrarias que conhece uma aspirante a escritora e usa a internet e as mídias sociais como ferramentas para reunir as informações pessoais para se aproximar dela e também para fazer a mulher dos seus sonhos se apaixonar por ele.");
+
+    System.out.println("");
+
+    System.out.println("************");
+    System.out.println("* Trailer *");
+    System.out.println("************");
+
+    System.out.println("");
+    System.out.println("Link : https://youtu.be/mNkyrdzzWok?si=P_6RY0SvGCkocYpU");
+  }
+
+  
+}
